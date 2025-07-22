4c3b4899d77bc299c7dc5be68b7ff33fc0931be1
devanathan471 <devanathans828@gmail.com>
2025-07-22 09:26:46 -0700

    Ifelsedemo

---
+++ C2TCJAVA/src/Day2/Ifelsedemo.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class Ifelsedemo {
+	public static void main(String[] args) {
+		int age=11;
+				
+				if(age>=18)
+					System.out.println(" You are eligible for Voting");	
+				else
+					System.out.println("You are not eligible");
+}
+}
