897a2755607abd9eb64bba55a90baf4479536e78
devanathan471 <devanathans828@gmail.com>
2025-07-22 09:07:41 -0700

    Identifiers

---
+++ C2TCJAVA/src/Day1/Identifiers.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Identifiers {
+	public static void main(String[] args) {
+			int $number = 10;
+			System.out.println("value of the number variable is : "+ $number);
+
+			String studentName="Deva"; 
+			System.out.println("value is : "+ studentName);
+
+}
+
+}
